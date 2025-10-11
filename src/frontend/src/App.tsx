8a433747aaac9b7ca1898c6448a173d3d66e238b
Nishidh Singh <xnishidh.codes@gmail.com>
2025-10-11 19:13:33 +0530

    added landing page icons

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./components/Landing";
 import ImageDetectionComponent from "./components/ImageDetectionComponent";
 import LiveDetectionComponent from "./components/LiveDetectionComponent";
+import Documentation from "./components/Documentation";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Landing />}></Route>
         <Route path={`/jenji/live`} element={<LiveDetectionComponent />} />
         <Route path="/jenji/predict/upload" element={<ImageDetectionComponent />}></Route>
+        <Route path="/jenji/docs" element={<Documentation />}></Route>
       </Routes>
     </BrowserRouter>
   );
